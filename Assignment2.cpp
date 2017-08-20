600ae5dd2198391bf14d9ee2b6254582c6b2ff19
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-20 23:30:31 +0530

    Add existing file

---
+++ Assignment2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a,b,c;
+ cout << "Enter a number=";
+ cin >> a;
+ cout << "Enter another number=";
+ cin >> b;
+ c=a+b;
+ cout << "Sum=" << c;
+
+ return 0;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS141
+Assignment1
+
