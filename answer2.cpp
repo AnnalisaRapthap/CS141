6b61245095f4cf277bf2da106cb1852a7e213d97
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-21 21:34:09 +0530

    Add existing file

---
+++ answer2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,s;
+    cout << "Enter two numbers" << endl;
+    cin >> a;
+    cin >> b;
+    s=a+b;
+    cout << "Sum=" << s;
+    return 0;
+}
