346496a25ac5d46f1c0153d7c4b7cefc84c58d6a
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-21 22:14:08 +0530

    Add existing file

---
+++ answer5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l,b,a;
+    cout << "Enter length of rectangle=" << endl;
+    cin >> l;
+    cout << "Enter breadth of rectangle=" << endl;
+    cin >> b;
+    a=l*b;
+    cout << "Area=" << a;
+    return 0;
+}
