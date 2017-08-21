ca578b9ac26932a954c0188a5742d96d875a2a34
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-21 22:11:11 +0530

    Add existing file

---
+++ answer4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l,b,p;
+    cout << "Enter length of rectangle=" << endl;
+    cin >> l;
+    cout << "Enter breadth of rectangle=" << endl;
+    cin >> b;
+    p=2*(l+b);
+    cout << "Perimeter=" << p;
+    return 0;
+}
