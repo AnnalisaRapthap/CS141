2d9038c872e3038300c9c14c623e615c82d4c2ad
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-21 21:11:00 +0530

    Add existing file

---
+++ answer1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    bool e=true;
+    bool f=false;
+
+    cout << "Enter integer=";
+    cin >> a;
+    cout << "Enter a float=";
+    cin >> c;
+    cout << "Enter any character=";
+    cin >> b;
+    cout << "Enter double=";
+    cin >> d;
+    cout << "Is Integer greater than float?";
+    if(a>c)
+    {
+
+
+        cout << e;
+    }
+        else
+            cout << f;
+
+return 0 ;
+}
