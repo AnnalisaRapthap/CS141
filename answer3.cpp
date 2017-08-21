9df9964b4630faa1f76c08709a6dae5d376044e3
AnnalisaRapthap <annalisa.rapthap@nisr.ac.in>
2017-08-21 22:04:42 +0530

    Add existing file

---
+++ answer3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a,b,s,d,m,q,i,j=1;
+    cout << "Enter two numbers" << endl;
+    cin >> a;
+    cin >> b;
+    for(j=1;j<5;j++)
+    {
+         cout << "Press 1 for sum, 2 for difference, 3 for product and 4 for quotient" << endl;
+    cin >> i;
+
+
+
+
+
+    if (i==1){
+        s=a+b;
+        cout << "Sum=" << s << endl;
+    }
+    else if (i==2){
+        d=a-b;
+        cout << "Difference=" << d << endl;
+    }
+    else if(i==3){
+        m=a*b;
+        cout << "Product=" << m << endl;
+    }
+    else if(i==4){
+        q=a/b;
+        cout << "Quotient=" << q << endl;
+    }
+
+    else
+        cout << "Error" << endl;
+
+}
+
+    return 0;
+}
+
